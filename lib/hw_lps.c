4c3a28029b1da8151605e3195c17a20912851a09
Uwe Hermann <uwe@hermann-uwe.de>
2010-02-19 23:28:56 +0100

    Code readability improvements.

--- lib/hw_lps.c
+++ lib/hw_lps.c
@@ -34,7 +34,7 @@ uint8_t *hw_lps_get_samples(struct flosslogic_context *ctx,
 			    uint64_t numsamples, uint64_t samplerate,
 			    int timeout)
 {
-	int ret;
+	int ret, bufsize;
 	uint64_t i;
 	char cmdbuf[5];
 	uint8_t *sample_buffer;
@@ -50,12 +50,13 @@ uint8_t *hw_lps_get_samples(struct flosslogic_context *ctx,
 	cmdbuf[4] = 0x0c;
 	ret = usb_interrupt_write(ctx->devhandle, 0x01, cmdbuf, 5, timeout);
 
-	sample_buffer = malloc(numsamples * (ctx->la->numchannels / 8));
+	bufsize = numsamples * (ctx->la->numchannels / 8);
+	sample_buffer = malloc(bufsize);
 	/* TODO: Better error handling? */
 	if (sample_buffer == NULL)
 		return NULL;
 
-	for (i = 0; i < numsamples * (ctx->la->numchannels / 8); i += 512) {
+	for (i = 0; (int)i < bufsize; i += 512) {
 		/* 0x86 == EP 6, IN direction */
 		ret = usb_block_read(ctx->devhandle, 0x86,
 			       (char *)(sample_buffer + i), 512, timeout);

6e9881fc992ff70c4d6770f182767499fb9d88ba
Uwe Hermann <uwe@hermann-uwe.de>
2010-03-02 16:20:48 +0100

    Use current directory as File->Open default dir.

--- gui/mainwindow.cpp
+++ gui/mainwindow.cpp
@@ -84,5 +84,5 @@ void MainWindow::on_actionScan_triggered()
 void MainWindow::on_action_Open_triggered()
 {
 	QString fileName = QFileDialog::getOpenFileName(this,
-		tr("Open sample file"), "/", tr("Raw sample files (*.bin)"));
+		tr("Open sample file"), ".", tr("Raw sample files (*.bin)"));
 }
